c6ced6cd2ae690aa014f809b39b26173b8895bd5
unix <unix.bio@gmail.com>
2020-03-23 07:53:00 +0800

    feat: add zeit-ui react

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ZEITUIProvider, CSSBaseline } from '@zeit-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ZEITUIProvider>
+      <CSSBaseline />
+      <App />
+    </ZEITUIProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
