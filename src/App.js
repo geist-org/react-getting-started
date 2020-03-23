7ca0d66505f03bac709feeb58538df325bc9d8ea
unix <unix.bio@gmail.com>
2020-03-23 08:10:21 +0800

    feat: first page for zeit-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card, Code, Note, Spacer } from '@zeit-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Card>
+        hello, world. I am using <Code>@zeit-ui/react</Code> !
+      </Card>
+      <Spacer y={1.5} />
+      <Card shadow>
+        <Note type="success">This note details something important.</Note>
+      </Card>
     </div>
   );
 }
