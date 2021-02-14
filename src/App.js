17cd5aafed156ddcffe76384d5ac1917676948b6
unix <unix.bio@gmail.com>
2021-02-14 16:46:46 +0800

    chore: simplify project and upgrade deps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import './App.css'
-import { Card, Code, Note, Spacer } from '@geist-ui/react'
+import { Text, Page } from '@geist-ui/react'
 
 function App() {
   return (
-    <div className="app">
-      <Card>
-        hello, world. I am using <Code>@geist-ui/react</Code> !
-      </Card>
-      <Spacer y={1.5} />
-      <Card shadow>
-        <Note type="success">This note details something important.</Note>
-      </Card>
-    </div>
+    <Page dotBackdrop size="mini">
+      <Page.Header>
+        <Text h2>React Application with Geist UI</Text>
+      </Page.Header>
+      <Text>
+        Hello, I am using <Text b>Gesit UI</Text> !
+      </Text>
+    </Page>
   )
 }
 
